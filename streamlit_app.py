9b6c61a4663c109d4d14d303936722d9ee84e64f
nour <46919831+nhayari@users.noreply.github.com>
2025-12-28 03:11:49 +0100

    Integrate pandas for DataFrame conversion
    Added pandas integration to convert Snowflake DataFrame to pandas DataFrame for further processing.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Custom Your Smoothie :cup_with_straw:")
@@ -17,7 +18,8 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 #my_dataframe = session.table("smoothies.public.fruit_options")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df =my_dataframe.to_pandas()
 
 
 
@@ -37,6 +39,9 @@ if ingredients_list :
         ingredients_string=''
         for each_fruit in ingredients_list:
           ingredients_string += each_fruit + ' '
+          search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
+          st.write('The search value for ', each_fruit,' is ', search_on, '.')
+         
           st.subheader(each_fruit+ ' Nutrition Information.')
           smoothiefruit_res = requests.get("https://my.smoothiefroot.com/api/fruit/"+each_fruit)
           sf_df = st.dataframe(data=smoothiefruit_res.json(),use_container_width=True)
